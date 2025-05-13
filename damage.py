fbfcfcd626890aa2ab196edd4e589851233cf555
= <=>
2025-05-13 13:46:23 +0100

    damage calculation and turn-based combat is success

--- damage.py
+++ damage.py
@@ -1,37 +1,56 @@
 import random
-import sys
 import time
-
-
 
 def battle():
     PlayerHP = 10
     EnemyHP = 10
-    while EnemyHP or PlayerHP > 0:
+    while EnemyHP > 0 and PlayerHP > 0: 
         print("[1] Attack [2] Defend [3] Run")
         choice = input("> ").strip()
         if choice == '1':
-            EnemyATK, PlayerATK = random.sample(range(1,5),2)
+            PlayerATK = random.randint(1, 5) #Initiate random attack inside the loop to ensure random number generation is truly random
+            EnemyATK = random.randint(1, 5)
+
             print("You swing your sword.")
-            EnemyHP = EnemyHP - PlayerATK
-            print("You hit the enemy.")
+            EnemyHP -= PlayerATK
             print(f"You dealt {PlayerATK} damage.")
             print(f"The enemy's HP is now: {EnemyHP}")
             time.sleep(1)
-            print("The enemy swings their sword at you.")
-            PlayerHP = PlayerHP - EnemyATK
-            print(f"The enemy hits you for {EnemyATK} damage.")
-            print(f"Your HP is {PlayerHP}.")
-            time.sleep(1)
+
             if EnemyHP <= 0:
                 print("You defeat the enemy.")
-                exit()
-            elif PlayerHP <= 0:
+                break
+
+            print("The enemy swings their sword at you.")
+            PlayerHP -= EnemyATK
+            print(f"The enemy hits you for {EnemyATK} damage.")
+            print(f"Your HP is now: {PlayerHP}")
+            time.sleep(1)
+
+            if PlayerHP <= 0:
                 print("The enemy defeats you.")
-                exit()
+                break
+        if choice == '2':
+            EnemyATK = random.randint(1, 3)
+            print("You fortify yourself.")
+            print("The enemy swings their sword at you.")
+            PlayerHP -= EnemyATK
+            print(f"The enemy hits you for {EnemyATK} damage.")
+            print(f"Your HP is now: {PlayerHP}")
+            time.sleep(1)
+
+            if PlayerHP <= 0:
+                print("The enemy defeats you.")
+                break
+
+        if choice == '3':
+            print("You flee from battle.")
+            break
+
 
 def main():
     battle()
 
 main()
+
     
